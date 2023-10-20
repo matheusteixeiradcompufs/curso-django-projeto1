d1e3c40951d0659b6d904df500acb3cd0e7a951e
matheusteixeiradcompufs <111207050+matheusteixeiradcompufs@users.noreply.github.com>
2023-10-19 23:03:46 -0300

    Aula 92

--- recipes/tests/test_recipe_model_recipe.py
+++ recipes/tests/test_recipe_model_recipe.py
@@ -1,5 +1,21 @@
+from parameterized import parameterized
+from django.core.exceptions import ValidationError
+
 from recipes.tests.test_recipe_base import RecipeTestBase
 
 
 class RecipeModelTest(RecipeTestBase):
-    pass
+    def setUp(self) -> None:
+        self.recipe = self.make_recipe()
+        return super().setUp()
+
+    @parameterized.expand([
+        ('title', 65),
+        ('description', 165),
+        ('preparation_time_unit', 65),
+        ('servings_unit', 65),
+    ])
+    def test_recipe_fields_max_length(self, field, max_length):
+        setattr(self.recipe, field, 'A' * (max_length + 1))
+        with self.assertRaises(ValidationError):
+            self.recipe.full_clean()
