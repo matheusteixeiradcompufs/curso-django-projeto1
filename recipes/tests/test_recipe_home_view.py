ae53b7367987e11a0768d7f095fc461245bef305
matheusteixeiradcompufs <111207050+matheusteixeiradcompufs@users.noreply.github.com>
2023-10-23 07:21:28 -0300

    Aula 111

---
+++ recipes/tests/test_recipe_home_view.py
@@ -0,0 +1,46 @@
+from django.urls import reverse, resolve
+
+from recipes import views
+from recipes.tests.test_recipe_base import RecipeTestBase
+
+
+class RecipeHomeViewTest(RecipeTestBase):
+    # tests to home views
+    def test_recipe_home_view_function_is_correct(self):
+        view = resolve(reverse('recipes:home'))
+        self.assertTrue(view.func, views.home)
+
+    def test_recipe_home_view_returns_status_code_200_OK(self):
+        response = self.client.get(reverse('recipes:home'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_recipe_home_view_loads_correct_template(self):
+        response = self.client.get(reverse('recipes:home'))
+        self.assertTemplateUsed(response, 'recipes/pages/home.html')
+
+    def test_recipe_home_template_shows_no_recipes_found_if_no_recipes(self):
+        response = self.client.get(reverse('recipes:home'))
+        self.assertIn(
+            '<h1>No recipes found here :(</h1>',
+            response.content.decode('utf-8')
+        )
+
+    def test_recipe_home_template_loads_recipes(self):
+        self.make_recipe()
+
+        response = self.client.get(reverse('recipes:home'))
+        content = response.content.decode('utf-8')
+        response_recipes = response.context['recipes']
+
+        self.assertIn('Titulo da Receita', content)
+        self.assertEqual(len(response_recipes), 1)
+
+    def test_recipe_home_template_load_recipes_not_published(self):
+        self.make_recipe(recipe_is_published=False)
+
+        response = self.client.get(reverse('recipes:home'))
+
+        self.assertIn(
+            '<h1>No recipes found here :(</h1>',
+            response.content.decode('utf-8')
+        )
--- recipes/views.py
+++ recipes/views.py
@@ -1,3 +1,5 @@
+from django.db.models import Q
+from django.http import Http404
 from django.shortcuts import render, get_object_or_404, get_list_or_404
 
 from recipes.models import Recipe
@@ -26,4 +28,25 @@ def recipe(request, recipe_id):
     return render(request, 'recipes/pages/recipe-view.html', context={
         'recipe': recipe_content,
         'is_detail_page': True
+    })
+
+
+def search(request):
+    search_term = request.GET.get('search', '').strip()
+
+    if not search_term:
+        raise Http404()
+
+    recipes = Recipe.objects.filter(
+        Q(
+            Q(title__icontains=search_term) |
+            Q(description__icontains=search_term),
+        ),
+        is_published=True
+    ).order_by('-id')
+
+    return render(request, 'recipes/pages/search.html', {
+        'page_title': f'Search for "{search_term}" |',
+        'search_term': search_term,
+        'recipes': recipes,
     })
