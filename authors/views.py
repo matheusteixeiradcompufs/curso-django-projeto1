5c3b4794024a60e3af43e530ad688b394d275e1a
Matheus Cardoso Teixeira <matheusc.teixeira@outlook.com>
2023-10-26 14:22:22 -0300

    Aula 135

--- authors/views.py
+++ authors/views.py
@@ -1,11 +1,12 @@
 from django.http import Http404
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from authors.forms import RegisterForm
 
 
 def register_view(request):
-    form = RegisterForm()
+    register_form_data = request.session.get('register_form_data', None)
+    form = RegisterForm(register_form_data)
     return render(request, 'authors/pages/register_view.html', {
         'form': form,
     })
@@ -15,7 +16,8 @@ def register_create(request):
     if not request.POST:
         raise Http404()
 
-    form = RegisterForm()
-    return render(request, 'authors/pages/register_view.html', {
-        'form': form,
-    })
+    post = request.POST
+    request.session['register_form_data'] = post
+    form = RegisterForm(post)
+    return redirect('authors:register')
+
